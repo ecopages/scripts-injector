f925e0a9366f40fe17d06f2481406230f1fd6b89
azanenghi <andrea.zanenghi@gmail.com>
2026-03-02 23:08:23 +0100

    chore(www): fix env typings and align workspace dependencies

--- apps/www/modules.d.ts
+++ apps/www/modules.d.ts
@@ -1,4 +1,20 @@
-/// <reference types="../../@ecopages/core/declarations" />
-/// <reference types="../../@ecopages/core/env" />
-/// <reference types="../../@ecopages/image-processor/types" />
+interface EcopagesEnv {
+	ECOPAGES_BASE_URL: string;
+	ECOPAGES_HOSTNAME: string;
+	ECOPAGES_PORT: string;
+	ECOPAGES_LOGGER_DEBUG: 'true' | 'false';
+}
 
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends EcopagesEnv {}
+	}
+
+	namespace Bun {
+		interface Env extends EcopagesEnv {}
+	}
+
+	interface ImportMetaEnv extends EcopagesEnv {}
+}
+
+export {};
