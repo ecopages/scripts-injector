54640913afa1b31f7ad7282b858497853805fce5
azanenghi <andrea.zanenghi@gmail.com>
2026-08-15 00:52:33 +0200

    feat(www): update favicon and SEO metadata for improved site branding

--- apps/www/src/includes/seo.tsx
+++ apps/www/src/includes/seo.tsx
@@ -6,10 +6,20 @@ export function Seo({ title, description, url, keywords }: PageMetadataProps) {
 	return (
 		<>
 			<title safe>{title}</title>
+			<link
+				rel="icon"
+				type="image/svg+xml"
+				href="/favicon/favicon-dark.svg"
+				media="(prefers-color-scheme: light)"
+			/>
+			<link
+				rel="icon"
+				type="image/svg+xml"
+				href="/favicon/favicon-light.svg"
+				media="(prefers-color-scheme: dark)"
+			/>
 			<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-			<link rel="manifest" href="site.webmanifest" />
+			<link rel="manifest" href="/favicon/site.webmanifest" />
 			<link rel="robots" href="/robots.txt" />
 			<meta name="description" content={description} />
 			{keywords?.length ? ((<meta name="keywords" content={keywords.join(',')} />) as 'safe') : null}
